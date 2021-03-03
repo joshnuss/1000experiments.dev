9498eadd77f76c62f7b51c7ae0f22d78de3fb2f3
Josh Nussbaum <joshnuss@gmail.com>
2021-03-03 02:21:55 -0500

    Adds post template generator

---
+++ scripts/generate.js
@@ -0,0 +1,50 @@
+const {format} = require('date-fns')
+const fs = require('fs')
+const glob = require('glob')
+const matter = require('gray-matter')
+const read = require('read')
+
+const experiment = lastExperiment() + 1
+const permalink = process.argv[2]
+
+function lastExperiment() {
+  let last = 0
+  let content
+
+  glob.sync('posts/*.md').forEach(file => {
+    content = fs.readFileSync(file, 'utf8')
+    const experiment = matter(content).data.experiment
+
+    if (experiment && experiment > last) {
+      last = experiment
+    }
+  })
+
+  return last
+}
+
+function generate({title, experiment, tags, permalink}) {
+  return `---
+title: ${title}
+experiment: ${experiment}
+date: ${format(new Date(), 'yyyy-MM-dd')}
+permalink: ${permalink}
+tags: ${tags}
+---
+
+Content here
+`
+}
+
+read({prompt: 'title:'}, function (err, title) {
+  read({prompt: 'tags:', default: ''}, function (err, tags) {
+    const template = generate({
+      title,
+      experiment,
+      permalink,
+      tags
+    })
+    console.log(template)
+    fs.writeFileSync(`posts/${experiment}-${permalink}.md`, template)
+  })
+})
