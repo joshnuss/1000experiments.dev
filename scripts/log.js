640fff16f8c9d29abfac9368619a37dc42af21fd
Josh Nussbaum <joshnuss@gmail.com>
2021-03-06 19:13:51 -0500

    Make output of log a markdown list

--- scripts/log.js
+++ scripts/log.js
@@ -3,5 +3,5 @@ const getPosts = require('./posts.js')
 getPosts().forEach(post => {
   const { experiment, permalink, title } = post.data
 
-  console.log(`[#${experiment} - ${title}](https://1000experiments.dev/posts/${permalink})`)
+  console.log(`- [#${experiment} - ${title}](https://1000experiments.dev/posts/${permalink})`)
 })
