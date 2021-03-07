73ab6d4bd051691f0a7decf78fab42e9a57ea11b
Josh Nussbaum <joshnuss@gmail.com>
2021-03-07 18:45:08 -0500

    Update style for mobile

--- src/components/PostList.svelte
+++ src/components/PostList.svelte
@@ -16,11 +16,19 @@
     display: inline-block;
   }
   a {
+    display: flex;
+    gap: 5px;
+    align-items: flex-start;
+
     text-decoration: none;
   }
   a h2 {
     text-decoration: underline;
     text-decoration-color: #ddd;
+    margin: 5px 0 0 0;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
   }
   a:hover h2 {
     color: var(--highlight-color);
