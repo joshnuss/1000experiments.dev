dcde423e119c193031b38622422240f1e3c3f95d
Josh Nussbaum <joshnuss@gmail.com>
2022-06-09 05:30:02 -0400

    Switches to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
 import sveltePreprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-vercel'
 
 export default {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
